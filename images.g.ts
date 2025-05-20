bfbbb6bd68ca7a095bd1ef8fd89dd36d8aff604f
Kalelium <144084222+Kalelium@users.noreply.github.com>
2025-05-20 15:01:14 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,141 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "myImage":return img`
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+eee4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444eee
+ee44cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc44ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccceeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeccccccccccc4ee
+ee4cccccccccceeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeecccccccccc4ee
+ee4ccccccccceee4444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444eeeccccccccc4ee
+ee4cccccccccee44................................................................................................................................44eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee4..................................................................................................................................4eeccccccccc4ee
+ee4cccccccccee55................................................................................................................................55eeccccccccc4ee
+ee4ccccccccceee5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555eeeccccccccc4ee
+ee4cccccccccceee44444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444eeecccccccccc4ee
+ee4cccccccccccee44444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444eeccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccceeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeccccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccceeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeecccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccceee4444444444444444444444444444444444444444444444444444444444444444444444444eeeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee44.......................................................................44eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee4.........................................................................4eeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccee55.......................................................................55eeccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccceee5555555555555555555555555555555555555555555555555555555555555555555555555eeeccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccceee44444444444444444444444444444444444444444444444444444444444444444444444eeecccccccccccccccccccccccccccccccccccccc4ee
+ee4ccccccccccccccccccccccccccccccccccccccccee44444444444444444444444444444444444444444444444444444444444444444444444eeccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee4cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc4ee
+ee55cccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc55ee
+eee5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555eee
+eeee44444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444eeee
+eeee44444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444444eeee
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
